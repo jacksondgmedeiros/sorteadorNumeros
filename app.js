a445cca72d82f5c0764a026bcb696c7ad5a19f2a
Jackson Medeiros <jackson.dgmedeiros@gmail.com>
2025-10-22 15:38:37 -0300

    Projeto desenvolvido do bootcamp do Santander em parceira com a ALura, onde foi fornecido o código html e css e com ajuda dos instrutores, realizei a criação do código no arquivo app.js. Nele, foi criado as funções para sortear os números, obter numeros aleatórios entre os valores passados, reiniciar o jogo, entre outras funções simples.

---
+++ app.js
@@ -0,0 +1,46 @@
+function sortear(){
+    let quantidade = parseInt(document.querySelector('#quantidade').value);
+    let de = parseInt(document.querySelector('#de').value);
+    let ate = parseInt(document.querySelector('#ate').value);
+
+
+    let sorteados = [];
+    for (let i = 0; i < quantidade; i++) {
+         let numero = obterNumeroAleatorio(de, ate);
+        while(sorteados.includes(numero)){
+
+            numero = obterNumeroAleatorio(de, ate);
+        }
+        sorteados.push(numero);
+        
+    }
+    
+     let resultado = document.getElementById('resultado');
+     resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${sorteados}</label>`;
+     alterarStatusBotao();
+
+}
+
+function obterNumeroAleatorio(min, max){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function alterarStatusBotao(){
+    let botao = document.getElementById('btn-reiniciar');
+    if(botao.classList.contains('container__botao-desabilitado')){
+        botao.classList.remove('container__botao-desabilitado');
+        botao.classList.add('container__botao');
+    }else{
+        botao.classList.remove('container__botao');
+        botao.classList.add('container__botao-desabilitado');
+
+    }
+}
+
+function reiniciar(){
+    document.querySelector('#quantidade').value = '';
+    document.querySelector('#de').value = '';
+    document.querySelector('#ate').value = '';
+    document.querySelector('#resultado').value = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>';
+    alterarStatusBotao();
+}
